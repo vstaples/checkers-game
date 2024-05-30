62a17973d2415402409d524f34789911425812bb
Vaughn Staples <vstaples64@gmail.com>
2024-05-30 19:32:29 +0800

    Added server.js & package.json

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// When a client connects
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Listen for a move
+    socket.on('move', (data) => {
+        // Broadcast the move to all other clients
+        socket.broadcast.emit('move', data);
+    });
+
+    // When the client disconnects
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
