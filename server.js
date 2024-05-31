bd24554c807f8699ac5c0d61a753d13ea1a54b75
Vaughn Staples <vstaples64@gmail.com>
2024-05-31 08:01:09 +0800

    Update checkers project

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // When a client connects
